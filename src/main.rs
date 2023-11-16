eb971f19fcb89ae678eac737db18e77fbd900143
Chris <yoda2031@gmail.com>
2023-11-16 23:25:25 +0000

    Add GUI with some configurable options

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
+use miniquad::window::screen_size;
 use macroquad::rand::{RandomRange,srand};
 use macroquad::ui::root_ui;
+use macroquad::ui::widgets::Window;
 use macroquad::prelude::*;
 use macroquad::color::hsl_to_rgb;
+use macroquad::hash;
 use std::fmt::Write;
 
-macro_rules! clamp {
-    ($value: expr, $min: expr, $max:expr) => {
-        std::cmp::min(std::cmp::max($value, $min), $max)
-    }
-}
 
-const DEFAULT_WIDTH:i32 = 1920;
-const DEFAULT_HEIGHT:i32 = 1080;
+const DEFAULT_WIDTH:f32 = 1920.0;
+const DEFAULT_HEIGHT:f32 = 1080.0;
 const EPSILON:f32 = 0.1;
 
 fn get_random_value(min: i32, max: i32) -> i32 {
@@ -21,8 +19,8 @@ fn get_random_value(min: i32, max: i32) -> i32 {
 fn conf() -> Conf {
     Conf {
         window_title: String::from("Jiggle Balls"),
-        window_width: DEFAULT_WIDTH,
-        window_height: DEFAULT_HEIGHT,
+        window_width: DEFAULT_WIDTH as i32,
+        window_height: DEFAULT_HEIGHT as i32,
         fullscreen: false,
         ..Default::default() 
     }
@@ -30,18 +28,23 @@ fn conf() -> Conf {
 
 #[macroquad::main(conf)]
 async fn main() {
-    let mut width:i32 = DEFAULT_WIDTH;
-    let mut height:i32 = DEFAULT_HEIGHT;
+    let mut width:f32 = DEFAULT_WIDTH;
+    let mut height:f32 = DEFAULT_HEIGHT;
+    let mut show_gui = false;
 
     let mut circles = Vec::new();
     let mut fullscreen_requested = false;
-    let mut jiggle = 3;
-    let mut draw_gui = true;
+    let mut is_fullscreen = false;
+    let mut jiggle:f32 = 3.0;
+    let mut mouse_repel_force = 2.0;
+    let mut mouse_attract_force = 0.15;
+    let mut mouse_attract_distance = 100.0;
+    let mut medium_viscosity: f32 = 1000.0;
     srand(get_time() as u64);
 
     for _i in 0..1000 {
-        let x = get_random_value(0, width);
-        let y = get_random_value(0, height);
+        let x = get_random_value(0, width as i32) as f32;
+        let y = get_random_value(0, height as i32) as f32;
         let mut h = get_random_value(0, 100) as f32;
         h /= 100.0;
         let color = hsl_to_rgb(h, 1.0, 0.5);
@@ -53,36 +56,36 @@ async fn main() {
     loop {
         let delta_time = get_frame_time();
 
+        (width, height) = screen_size();
         if root_ui().active_window_focused() {
             show_mouse(false);
         }
         if fullscreen_requested {
-            set_fullscreen(true);
-            width = screen_width() as i32;
-            height = screen_height() as i32;
+            is_fullscreen = !is_fullscreen;
+            set_fullscreen(is_fullscreen);
             // maybe not needed?
-            //rl.set_window_size(width, height);
+            request_new_screen_size(width, height);
             fullscreen_requested = !fullscreen_requested;
         }
 
         if is_key_pressed(KeyCode::Minus) {
-            jiggle -= 1;
+            jiggle -= 1.0;
         }
 
         if is_key_pressed(KeyCode::Equal) {
-            jiggle += 1;
+            jiggle += 1.0;
         }
 
         if is_key_pressed(KeyCode::Q) {
             break;
         }
 
-        if is_key_pressed(KeyCode::G) {
-            draw_gui = !draw_gui;
-        }
-        
         if is_key_pressed(KeyCode::F) {
             fullscreen_requested = !fullscreen_requested;
+        }
+
+        if is_key_pressed(KeyCode::G) {
+            show_gui = !show_gui;
         }
 
         clear_background(Color::from_rgba(0x00, 0x00, 0x00, 0xC0));
@@ -92,21 +95,20 @@ async fn main() {
             let (x, y, circle_size, color, velocity) = circ;
             // draw the circle before doing anything else - everything else is setting up for the
             // next frame
-            draw_circle(*x as f32, *y as f32, *circle_size, *color);
-            let jiggle_x:i32 = get_random_value(-jiggle, jiggle);
-            let jiggle_y:i32 = get_random_value(-jiggle, jiggle);
+            draw_circle(*x, *y, *circle_size, *color);
+            let jiggle_x:f32 = get_random_value(-(jiggle as i32), jiggle as i32) as f32;
+            let jiggle_y:f32 = get_random_value(-(jiggle as i32), jiggle as i32) as f32;
             let mut new_x = *x;
             let mut new_y = *y;
             let mut new_velocity = velocity.clone();
-            const DRAG_COEFFICIENT: f32 = 1000.0;
 
             // "drag" simulation
             if velocity.x != 0.0 {
-                new_x += (velocity.x * delta_time).round() as i32;
+                new_x += (velocity.x * delta_time).round();
                 if new_velocity.x < 0.0 {
-                    new_velocity.x += (EPSILON * delta_time * DRAG_COEFFICIENT).powi(2);
+                    new_velocity.x += (EPSILON * delta_time * medium_viscosity).powi(2);
                 } else {
-                    new_velocity.x -= (EPSILON * delta_time * DRAG_COEFFICIENT).powi(2);
+                    new_velocity.x -= (EPSILON * delta_time * medium_viscosity).powi(2);
                 }
                 // if we're within EPSILON of zero, make us zero instead
                 // this stops us flapping around zero
@@ -117,11 +119,11 @@ async fn main() {
 
             // "drag" simulation
             if velocity.y != 0.0 {
-                new_y += (velocity.y * delta_time).round() as i32;
+                new_y += (velocity.y * delta_time).round();
                 if new_velocity.y < 0.0 {
-                    new_velocity.y += (EPSILON * delta_time * DRAG_COEFFICIENT).powi(2);
+                    new_velocity.y += (EPSILON * delta_time * medium_viscosity).powi(2);
                 } else {
-                    new_velocity.y -= (EPSILON * delta_time * DRAG_COEFFICIENT).powi(2);
+                    new_velocity.y -= (EPSILON * delta_time * medium_viscosity).powi(2);
                 }
                 // if we're within EPSILON of zero, make us zero instead
                 // this stops us flapping around zero
@@ -140,41 +142,44 @@ async fn main() {
                 let (other_x, other_y, other_size, _, _) = other;
                 let x_dist = other_x - *x;
                 let y_dist = other_y - *y;
-                let dist = ((x_dist.pow(2) + y_dist.pow(2)) as f32).sqrt();
+                let dist = ((x_dist.powi(2) + y_dist.powi(2))).sqrt();
                 if dist < (*circle_size + other_size) {
                     new_velocity.x -= x_dist as f32;
                     new_velocity.y -= y_dist as f32;
                 }
             }
 
-            let mut mouse_gravity = 0.0;
-            let mut mouse_distance = 100.0;
-            // left click to invert gravity
-            if is_mouse_button_down(MouseButton::Left) {
-                mouse_gravity = -0.15;
-                mouse_distance *= 3.0;
-            } else if is_mouse_button_down(MouseButton::Right) {
-                mouse_gravity = 2.0;
-            }
-            let mouse_x_dist = *x - mouse_x as i32;
-            let mouse_y_dist = *y - mouse_y as i32;
-            let mouse_dist = ((mouse_x_dist.pow(2) + mouse_y_dist.pow(2)) as f32).sqrt();
-            if mouse_dist < mouse_distance {
-                new_velocity += Vec2::new(mouse_x_dist as f32, mouse_y_dist as f32) * mouse_gravity;
+            // disable the mouse interaction while the gui is on screen
+            if !show_gui {
+                let mut mouse_gravity = 0.0;
+                let mut mouse_distance = mouse_attract_distance;
+                // left click to invert gravity
+                if is_mouse_button_down(MouseButton::Left) {
+                    mouse_gravity = -mouse_attract_force;
+                    mouse_distance *= 3.0;
+                } else if is_mouse_button_down(MouseButton::Right) {
+                    mouse_gravity = mouse_repel_force;
+                }
+                let mouse_x_dist = *x - mouse_x;
+                let mouse_y_dist = *y - mouse_y;
+                let mouse_dist = ((mouse_x_dist.powi(2) + mouse_y_dist.powi(2)) as f32).sqrt();
+                if mouse_dist < mouse_distance {
+                    new_velocity += Vec2::new(mouse_x_dist as f32, mouse_y_dist as f32) * mouse_gravity;
+                }
             }
 
             new_x += jiggle_x;
-            if new_x >= width-*circle_size as i32 || new_x <= *circle_size as i32 {
+            if new_x >= width - *circle_size || new_x <= *circle_size {
                 new_velocity.x = -new_velocity.x;
             }
 
             new_y += jiggle_y;
-            if new_y >= height-*circle_size as i32 || new_y <= *circle_size as i32 {
+            if new_y >= height - *circle_size || new_y <= *circle_size {
                 new_velocity.y = -new_velocity.y;
             }
                 
-            (clamp!(new_x, *circle_size as i32, width-*circle_size as i32), 
-             clamp!(new_y, *circle_size as i32, height-*circle_size as i32),
+            (new_x.clamp(*circle_size, width - *circle_size), 
+             new_y.clamp(*circle_size, height - *circle_size),
              *circle_size,
              *color,
              new_velocity)
@@ -186,9 +191,51 @@ async fn main() {
         let mut s = String::new();
         write!(s, "Jiggle: {jiggle}").unwrap();
         draw_text(s.as_str(), 0.0, 20.0, 32.0, BLUE);
+        s.clear();
         let fps = get_fps();
+        s.clear();
         write!(s, "FPS: {fps}").unwrap();
         draw_text(s.as_str(), 0.0, 52.0, 32.0, BLUE);
+
+        if show_gui {
+            Window::new(hash!(), vec2(20., 20.), vec2(420., 700.))
+                .label("Controls")
+                .close_button(false)
+                .ui(&mut root_ui(), |ui| {
+                    ui.slider(
+                        hash!(),
+                        "Jiggle",
+                        0.0 .. 100.0,
+                        &mut jiggle,
+                    );
+                    ui.slider(
+                        hash!(),
+                        "Mouse Repel Force",
+                        1.0 .. 5.0,
+                        &mut mouse_repel_force,
+                    );
+                    ui.slider(
+                        hash!(),
+                        "Mouse Attract Force",
+                        0.05 .. 1.0,
+                        &mut mouse_attract_force,
+                    );
+                    ui.slider(
+                        hash!(),
+                        "Mouse Attract Distance",
+                        1.0 .. 500.0,
+                        &mut mouse_attract_distance,
+                    );
+                    ui.slider(
+                        hash!(),
+                        "Viscosity of Medium",
+                        50.0 .. 2500.0,
+                        &mut medium_viscosity,
+                    );
+                });
+        }
+
+
         next_frame().await;
     }
 }
